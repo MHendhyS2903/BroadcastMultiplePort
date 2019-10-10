2a40901beb621623d428495d565b7af2ec768942
MHendhyS2903 <m.hendhy.s@gmail.com>
2019-10-10 14:10:40 +0700

    First Commit

---
+++ Wave pool/Form1.Designer.vb
@@ -0,0 +1,341 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txtDeelay = New System.Windows.Forms.TextBox()
+        Me.lblJumlahRow = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.btnSend = New System.Windows.Forms.Button()
+        Me.txtNumber = New System.Windows.Forms.TextBox()
+        Me.txtMessage = New System.Windows.Forms.TextBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.btnUpload = New System.Windows.Forms.Button()
+        Me.txtFile = New System.Windows.Forms.TextBox()
+        Me.btnBrowse = New System.Windows.Forms.Button()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.GroupBox3 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnClose = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.cmbPort = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox3.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.txtDeelay)
+        Me.GroupBox1.Controls.Add(Me.lblJumlahRow)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.btnSend)
+        Me.GroupBox1.Controls.Add(Me.txtNumber)
+        Me.GroupBox1.Controls.Add(Me.txtMessage)
+        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupBox1.Location = New System.Drawing.Point(641, 22)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(324, 352)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(6, 21)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(46, 24)
+        Me.ComboBox1.TabIndex = 9
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(58, 24)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(62, 16)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Seconds"
+        '
+        'txtDeelay
+        '
+        Me.txtDeelay.Location = New System.Drawing.Point(274, 11)
+        Me.txtDeelay.Name = "txtDeelay"
+        Me.txtDeelay.Size = New System.Drawing.Size(38, 22)
+        Me.txtDeelay.TabIndex = 6
+        Me.txtDeelay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+        '
+        'lblJumlahRow
+        '
+        Me.lblJumlahRow.AutoSize = True
+        Me.lblJumlahRow.Location = New System.Drawing.Point(9, 330)
+        Me.lblJumlahRow.Name = "lblJumlahRow"
+        Me.lblJumlahRow.Size = New System.Drawing.Size(81, 16)
+        Me.lblJumlahRow.TabIndex = 5
+        Me.lblJumlahRow.Text = "Jumlah Row"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 56)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(96, 16)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Input Message"
+        '
+        'btnSend
+        '
+        Me.btnSend.Location = New System.Drawing.Point(12, 243)
+        Me.btnSend.Name = "btnSend"
+        Me.btnSend.Size = New System.Drawing.Size(75, 23)
+        Me.btnSend.TabIndex = 1
+        Me.btnSend.Text = "Send"
+        Me.btnSend.UseVisualStyleBackColor = True
+        '
+        'txtNumber
+        '
+        Me.txtNumber.Location = New System.Drawing.Point(12, 272)
+        Me.txtNumber.Name = "txtNumber"
+        Me.txtNumber.Size = New System.Drawing.Size(132, 22)
+        Me.txtNumber.TabIndex = 2
+        '
+        'txtMessage
+        '
+        Me.txtMessage.Location = New System.Drawing.Point(6, 75)
+        Me.txtMessage.Multiline = True
+        Me.txtMessage.Name = "txtMessage"
+        Me.txtMessage.Size = New System.Drawing.Size(306, 140)
+        Me.txtMessage.TabIndex = 0
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.DataGridView1)
+        Me.GroupBox2.Controls.Add(Me.btnUpload)
+        Me.GroupBox2.Controls.Add(Me.txtFile)
+        Me.GroupBox2.Controls.Add(Me.btnBrowse)
+        Me.GroupBox2.Location = New System.Drawing.Point(309, 22)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(326, 352)
+        Me.GroupBox2.TabIndex = 1
+        Me.GroupBox2.TabStop = False
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AllowUserToAddRows = False
+        Me.DataGridView1.AllowUserToDeleteRows = False
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(6, 68)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.ReadOnly = True
+        Me.DataGridView1.Size = New System.Drawing.Size(234, 278)
+        Me.DataGridView1.TabIndex = 3
+        '
+        'btnUpload
+        '
+        Me.btnUpload.Location = New System.Drawing.Point(246, 39)
+        Me.btnUpload.Name = "btnUpload"
+        Me.btnUpload.Size = New System.Drawing.Size(75, 23)
+        Me.btnUpload.TabIndex = 2
+        Me.btnUpload.Text = "Upload"
+        Me.btnUpload.UseVisualStyleBackColor = True
+        '
+        'txtFile
+        '
+        Me.txtFile.Location = New System.Drawing.Point(6, 40)
+        Me.txtFile.Name = "txtFile"
+        Me.txtFile.Size = New System.Drawing.Size(234, 22)
+        Me.txtFile.TabIndex = 1
+        '
+        'btnBrowse
+        '
+        Me.btnBrowse.Location = New System.Drawing.Point(6, 11)
+        Me.btnBrowse.Name = "btnBrowse"
+        Me.btnBrowse.Size = New System.Drawing.Size(75, 23)
+        Me.btnBrowse.TabIndex = 0
+        Me.btnBrowse.Text = "Browse..."
+        Me.btnBrowse.UseVisualStyleBackColor = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'SerialPort1
+        '
+        '
+        'GroupBox3
+        '
+        Me.GroupBox3.Controls.Add(Me.Label3)
+        Me.GroupBox3.Controls.Add(Me.btnClose)
+        Me.GroupBox3.Controls.Add(Me.ListBox1)
+        Me.GroupBox3.Controls.Add(Me.Label2)
+        Me.GroupBox3.Controls.Add(Me.btnOpen)
+        Me.GroupBox3.Controls.Add(Me.cmbPort)
+        Me.GroupBox3.Location = New System.Drawing.Point(45, 22)
+        Me.GroupBox3.Name = "GroupBox3"
+        Me.GroupBox3.Size = New System.Drawing.Size(258, 352)
+        Me.GroupBox3.TabIndex = 5
+        Me.GroupBox3.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(10, 78)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(59, 16)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Com List"
+        '
+        'btnClose
+        '
+        Me.btnClose.Location = New System.Drawing.Point(6, 251)
+        Me.btnClose.Name = "btnClose"
+        Me.btnClose.Size = New System.Drawing.Size(75, 23)
+        Me.btnClose.TabIndex = 4
+        Me.btnClose.Text = "Close"
+        Me.btnClose.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 16
+        Me.ListBox1.Location = New System.Drawing.Point(6, 97)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(246, 148)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(6, 18)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(63, 16)
+        Me.Label2.TabIndex = 2
+        Me.Label2.Text = "Com Port"
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Location = New System.Drawing.Point(177, 38)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(75, 23)
+        Me.btnOpen.TabIndex = 1
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'cmbPort
+        '
+        Me.cmbPort.FormattingEnabled = True
+        Me.cmbPort.Location = New System.Drawing.Point(6, 37)
+        Me.cmbPort.Name = "cmbPort"
+        Me.cmbPort.Size = New System.Drawing.Size(165, 24)
+        Me.cmbPort.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(45, 595)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(74, 24)
+        Me.Button1.TabIndex = 6
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(125, 597)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(100, 22)
+        Me.TextBox2.TabIndex = 7
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(140, 490)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(8, 22)
+        Me.TextBox1.TabIndex = 8
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.WindowFrame
+        Me.ClientSize = New System.Drawing.Size(1012, 393)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.GroupBox3)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Broadcast"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox3.ResumeLayout(False)
+        Me.GroupBox3.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents btnSend As Button
+    Friend WithEvents txtMessage As TextBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents btnUpload As Button
+    Friend WithEvents txtFile As TextBox
+    Friend WithEvents btnBrowse As Button
+    Friend WithEvents txtNumber As TextBox
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents lblJumlahRow As Label
+    Friend WithEvents GroupBox3 As GroupBox
+    Friend WithEvents btnClose As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents btnOpen As Button
+    Friend WithEvents cmbPort As ComboBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtDeelay As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents TextBox1 As TextBox
+End Class
---
+++ Wave pool/Form1.vb
@@ -0,0 +1,212 @@
+﻿Imports System.IO
+Imports System.IO.Ports
+Imports System.Threading
+Imports System.Threading.Thread
+
+Public Class Form1
+    Private Tunda, deelay, terkirim As Integer
+    Private WithEvents COMport1 As New SerialPort
+    Private WithEvents COMport2 As New SerialPort
+    Private WithEvents COMport3 As New SerialPort
+    Private WithEvents COMport4 As New SerialPort
+    Private WithEvents COMport5 As New SerialPort
+    Private WithEvents COMport6 As New SerialPort
+    Private WithEvents COMport7 As New SerialPort
+    Private WithEvents COMport8 As New SerialPort
+    Delegate Sub SetTextCallback(ByVal [text] As String)
+    Dim thedatatable As New DataTable
+    Dim myList As New List(Of String)()
+    Dim myListPort As New List(Of String)()
+    Dim myPort As Array
+    Dim Ports As New List(Of IO.Ports.SerialPort)
+    Dim arrayPort As Integer = 0
+    Dim countNumber As Integer = 0
+    Dim validationCount, comLoop As Integer
+
+    Private filePath As String = "E:\ASIA TRANS\Wave pool\log.txt"
+
+    Private Sub BtnBrowse_Click(sender As Object, e As EventArgs) Handles btnBrowse.Click
+        If (OpenFileDialog1.ShowDialog() = DialogResult.OK) Then
+            txtFile.Text = OpenFileDialog1.FileName
+        End If
+    End Sub
+
+    Private Sub BtnUpload_Click(sender As Object, e As EventArgs) Handles btnUpload.Click
+        Dim thereader As New IO.StreamReader(txtFile.Text.ToString, System.Text.Encoding.Default)
+        Dim sline As String
+        Do
+            sline = thereader.ReadLine
+            If sline Is Nothing Then Exit Do
+            Dim thecolumns() As String = sline.Split(",")
+            Dim newrow As DataRow = thedatatable.NewRow
+            newrow("nomor") = thecolumns(0)
+            thedatatable.Rows.Add(newrow)
+        Loop
+        thereader.Close()
+
+        DataGridView1.DataSource = thedatatable
+        lblJumlahRow.Text = terkirim & " / " & thedatatable.Rows.Count & "rows"
+    End Sub
+
+    Private Sub BtnSend_Click(sender As Object, e As EventArgs) Handles btnSend.Click
+
+        deelay = (ComboBox1.Text * 1000) / myList.Count
+
+        validationCount = DataGridView1.Rows.Count
+
+        For x = 0 To myList.Count - 1
+            Ports.Add(New SerialPort)
+            Ports(x).PortName = myList(x)
+            Ports(x).BaudRate = 115200
+            Ports(x).WriteTimeout = 2000
+            If Not Ports(x).IsOpen Then
+                Ports(x).Open()
+                Ports(x).Write("AT+CMGF=1" & Chr(13))
+            Else
+                Ports(x).Close()
+                Ports(x).Open()
+            End If
+        Next
+
+        Button1.Enabled = False
+
+        With DataGridView1
+            If validationCount = 0 Then
+                MsgBox("Data Belum di Upload")
+            Else
+                If myList.Count > 0 Then
+                    Do
+
+
+                        If arrayPort > (myList.Count - 1) Then
+                            arrayPort = 0
+                            sendSms()
+                        Else
+                            sendSms()
+                        End If
+
+                    Loop Until countNumber > (DataGridView1.Rows.Count - 1)
+                    MsgBox("Pesan Telah Selesai Terkirim. ")
+                    Button1.Enabled = True
+                End If
+            End If
+        End With
+    End Sub
+
+    Public Sub sendSms()
+        Dim textAppend As String
+        Dim str As String = DataGridView1.Rows(countNumber).Cells("nomor").Value.ToString()
+        Dim result As String = str(0)
+        Try
+            If result = "8" Then
+                txtNumber.Clear()
+                txtNumber.Text = "0" & str
+            ElseIf result = "6" Then
+                txtNumber.Clear()
+                txtNumber.Text = "+" & str
+            Else
+                txtNumber.Clear()
+                txtNumber.Text = str
+            End If
+
+            Sleep(300)
+            Tunda = 300
+            Sleep(Tunda)
+
+
+            Application.DoEvents()
+
+            Ports(arrayPort).Write("AT+CMGS=" & Chr(34) & txtNumber.Text & Chr(34) & Chr(13))
+            Sleep(Tunda)
+            Ports(arrayPort).Write(txtMessage.Text & Chr(26))
+            terkirim += 1
+            countNumber += 1
+
+            lblJumlahRow.Text = terkirim & " / " & thedatatable.Rows.Count & "Numbers"
+
+            textAppend = txtNumber.Text + " " + "Terkirim " + DateTime.Now + vbCrLf
+
+
+            Try
+                File.AppendAllText(filePath, textAppend)
+            Catch ex As Exception
+                MsgBox(ex)
+            End Try
+
+            arrayPort += 1
+            Sleep(deelay)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        myPort = IO.Ports.SerialPort.GetPortNames()
+        cmbPort.Items.AddRange(myPort)
+
+        With thedatatable
+            .Columns.Add("nomor", System.Type.GetType("System.String"))
+        End With
+
+        ComboBox1.Items.Add("10")
+        ComboBox1.Items.Add("15")
+        ComboBox1.Items.Add("20")
+        ComboBox1.Items.Add("25")
+
+
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(sender As System.Object, e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        ReceivedText(Ports(ListBox1.SelectedIndex).ReadExisting())
+    End Sub
+
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        System.Threading.Thread.Sleep(1000)
+        If Ports(ListBox1.SelectedIndex).IsOpen Then
+            Ports(ListBox1.SelectedIndex).Close()
+            Ports.RemoveAt(ListBox1.SelectedIndex)
+        Else
+            MsgBox("port 0")
+        End If
+    End Sub
+    Private Sub BtnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        myList.Add(cmbPort.Text)
+        cmbPort.Items.Remove(cmbPort.Text)
+        ListBox1.Items.Clear()
+        Dim a As Integer = myList.Count
+        ListBox1.Items.Clear()
+        Dim ulang = 0
+        Do
+            ListBox1.Items.Add(myList(ulang).ToString)
+            ulang += 1
+        Loop Until ulang = a
+        TextBox2.Text = myList.Count
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        txtNumber.Text = myList(1)
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs)
+        Dim countNumber As Integer = 0
+        With DataGridView1
+            Do
+
+                countNumber += 1
+            Loop Until countNumber > (DataGridView1.Rows.Count - 1)
+        End With
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs)
+        COMport1.Close()
+    End Sub
+
+    Private Sub ReceivedText(ByVal [text] As String) 'input from ReadExisting
+        If Me.TextBox1.InvokeRequired Then
+            Dim x As New SetTextCallback(AddressOf ReceivedText)
+            Me.Invoke(x, New Object() {(text)})
+        Else
+            Me.txtMessage.Text &= [text] 'append text
+        End If
+    End Sub
+End Class
